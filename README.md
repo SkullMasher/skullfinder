ca9613c5edce96a4277d92439604d61bc474225a
Skullmasher <florian@skullmasher.io>
2018-12-12 10:22:02 +0100

    :skull: Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Skull Finder
+Où est Skullmasher ? Il est où Flo !
+
+## Pourquoi cette application éxiste
+Parce qu'on me demende souvent **"Tu es ou Flo ?"**. Mais surtout parce que j'ai envie de créer une applcation !
---
+++ app/js/script.js
@@ -0,0 +1,30 @@
+const greetingMessage = () => {
+  console.log('  #####   ')
+  console.log(' #######  ')
+  console.log('#  ###  # ' + ' Hello There')
+  console.log('#   #   # ')
+  console.log('######### ' + ' Come contribute to the code !')
+  console.log(' ### ###  ')
+  console.log('  #####   ' + ' github.com/SkullMasher/skullfinder')
+  console.log('  # # #   ')
+}
+
+const skullLatLng = [43.531127, 5.446]
+const markerText = 'Florian est à Aix en Provence !'
+
+let initMap = () => {
+  let map = L.map('map').setView(skullLatLng, 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map)
+
+  L.marker(skullLatLng).addTo(map)
+    .bindPopup(markerText)
+    .openPopup()
+}
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  greetingMessage()
+  initMap()
+});
