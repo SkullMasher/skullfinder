3ff38c7f99e4bda4caa910c788d60b9a94aa15df
Skullmasher <skullm4sher@gmail.com>
2018-12-13 18:39:44 +0100

    :skull: Change cursor position on map click

--- admin/js/script.js
+++ admin/js/script.js
@@ -9,10 +9,12 @@ const greetingMessage = () => {
   console.log('  # # #   ')
 }
 
-const skullLatLng = [43.531127, 5.446]
-const markerText = 'Florian est à Aix en Provence !'
 
 let initMap = () => {
+  const skullLatLng = [43.531127, 5.446]
+  const markerText = 'Florian est à Aix en Provence !'
+  let $form = document.getElementById('formMarker')
+
   let map = L.map('map',{
     zoomControl: true
   }).setView(skullLatLng, 13);
@@ -21,9 +23,13 @@ let initMap = () => {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map)
 
-  L.marker(skullLatLng).addTo(map)
+  let skullMarker = new L.marker(skullLatLng).addTo(map)
     .bindPopup(markerText)
     .openPopup()
+
+  map.on('click', (e) => {
+    skullMarker.setLatLng(e.latlng)
+  });
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
