590b310cda5d3519993e1b222d69a26138fc6c53
Skullmasher <skullm4sher@gmail.com>
2018-12-19 10:42:53 +0100

    :skull: You should commit more often

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,78 +1,179 @@
 const greetingMessage = () => {
-  console.log('  #####   ')
-  console.log(' #######  ')
-  console.log('#  ###  # ' + ' Hello There')
-  console.log('#   #   # ')
-  console.log('######### ' + ' Come contribute to the code !')
-  console.log(' ### ###  ')
-  console.log('  #####   ' + ' github.com/SkullMasher/skullfinder')
-  console.log('  # # #   ')
+  console.log(`  #####   `)
+  console.log(` #######  `)
+  console.log(`#  ###  #  Hello There`)
+  console.log(`#   #   # `)
+  console.log(`#########  Come contribute to the code !`)
+  console.log(` ### ###  `)
+  console.log(`  #####    github.com/SkullMasher/skullfinder`)
+  console.log(`  # # #   `)
 }
 
-let isNumeric = (n) => {
-  return !isNaN(parseFloat(n)) && isFinite(n)
+const db = { // Yeah I know
+  'remote': '127.0.0.1:5984',
+  'name': 'skullfinder_marker',
+  'user': 'skullfinder-editor',
+  'pass': 'topkek'
+}
+// Init the local and remote database
+const markerDb = new PouchDB(db.name)
+const remoteDbURL = `http://${db.user}:${db.pass}@${db.remote}/${db.name}`
+
+let initSync = () => {
+  let sync = PouchDB.sync(db.name, remoteDbURL, {
+    live: true,
+    retry: true
+  }).on('change', info => {
+    // handle change
+    console.log('Change Information : ', info)
+  }).on('paused', err => {
+    // replication paused (e.g. replication up to date, user went offline)
+    console.log('Paused (replication up to date, user went offline) : ', err)
+  }).on('active', () => {
+    // replicate resumed (e.g. new changes replicating, user went back online)
+    console.log('Active')
+  }).on('denied', err => {
+    // a document failed to replicate (e.g. due to permissions)
+    console.log('Denied : ', err)
+  }).on('complete', info => {
+    // handle complete
+    console.log('Complete info : ', info)
+  }).on('error', err => {
+    // handle error
+    console.log('err : ', err)
+  })
 }
 
-const dbName = 'skullfinder_marker'
-const remoteDbURL = 'http://127.0.0.1:5984/'
-
-var sync = PouchDB.sync(dbName, remoteDbURL, {
-  live: true,
-  retry: true
-}).on('change', (info) => {
-  // handle change
-  console.log('Change Information : ', info)
-}).on('paused', (err) => {
-  // replication paused (e.g. replication up to date, user went offline)
-  console.log('Paused err : ', err)
-}).on('active', () => {
-  // replicate resumed (e.g. new changes replicating, user went back online)
-  console.log('Active')
-}).on('denied', (err) => {
-  // a document failed to replicate (e.g. due to permissions)
-  console.log('Denied : ', err)
-}).on('complete', (info) => {
-  // handle complete
-  console.log('Complete info : ', info)
-}).on('error', (err) => {
-  // handle error
-  console.log('err : ', err)
-})
+let map
+let skullMarker
 
 let initMap = () => {
-  const skullLatLng = [43.531127, 5.446]
-  const markerText = 'Florian est à Aix en Provence !'
-
   let $formMarker = document.getElementById('formMarker')
   let $inputLat = $formMarker[0]
   let $inputLng = $formMarker[1]
   let $inputText = $formMarker[2]
   let $inputSubmit = $formMarker[3]
 
-  $inputLat.value = skullLatLng[0]
-  $inputLng.value = skullLatLng[1]
-  $inputText.value = markerText
+  markerDb.get('currentMarker')
+    .then(res => {
+      $inputLat.value = res.lat
+      $inputLng.value = res.lng
+      $inputText.value = res.text
 
-  let map = L.map('map', {
-    zoomControl: true
-  }).setView(skullLatLng, 13)
+      map = L.map('map', {
+        zoomControl: true
+      }).setView([res.lat, res.lng], 13)
 
-  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map)
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map)
 
-  let skullMarker = new L.marker(skullLatLng).addTo(map)
-    .bindPopup(markerText)
+      skullMarker = new L.marker([res.lat, res.lng]).addTo(map)
+        .bindPopup(res.text)
+        .openPopup()
+
+      map.on('click', (event) => {
+        skullMarker.setLatLng(event.latlng)
+        $inputLat.value = event.latlng.lat
+        $inputLng.value = event.latlng.lng
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+
+let showAllDocs = () => {
+  markerDb.allDocs({ include_docs: true })
+    .then((res) => {
+      console.log(res)
+    })
+    .catch((err) => {
+      console.error(err)
+    })
+}
+
+let mapFlyTo = (location) => {
+  let lat = location.lat
+  let lng = location.lng
+
+  skullMarker.setLatLng([lat, lng])
+    .bindPopup(location.text)
     .openPopup()
 
-  map.on('click', (e) => {
-    skullMarker.setLatLng(e.latlng)
-    $inputLat.value = e.latlng.lat
-    $inputLng.value = e.latlng.lng
+  map.flyTo([lat, lng], 13)
+}
+
+let setFeedback = (text, state) => {
+  let elmt = document.querySelector('.js-feedbackText')
+  let colorClass = [
+    'color--success', 'color--error', 'color--warn', 'color--muted'
+  ]
+
+  elmt.classList.remove(...colorClass)
+  switch (state) {
+    case 'success':
+      elmt.classList.add('color--success')
+      break
+    case 'warn':
+      elmt.classList.add('color--warn')
+      break
+    case 'error':
+      elmt.classList.add('color--error')
+      break
+    case 'muted':
+      elmt.classList.add('color--muted')
+      break
+  }
+
+  elmt.textContent = text
+}
+
+let initFastMarker = () => {
+  let buttons = document.querySelectorAll('.js-fastmarkers button')
+
+  Array.prototype.forEach.call(buttons, (btn, index) => {
+    btn.addEventListener('click', (event) => {
+      let location = btn.dataset.markername
+      // Put the info into the currentMarker
+      markerDb.bulkGet({
+        docs: [
+          { id: 'currentMarker' },
+          { id: location }
+        ]
+      })
+        .then((res) => {
+          let updatedMarker = res.results[0].docs[0].ok
+          let newLocation = res.results[1].docs[0].ok
+
+          if (updatedMarker.lat === newLocation.lat) {
+            let feedbackText = `Marker already set on ${newLocation._id}`
+            return setFeedback(feedbackText, 'warn')
+          } else {
+            updatedMarker.lat = newLocation.lat
+            updatedMarker.lng = newLocation.lng
+            updatedMarker.text = newLocation.text
+            // Weird but Nolan told me it's ok
+            // https://pouchdb.com/2015/05/18/we-have-a-problem-with-promises.html
+            return markerDb.put(updatedMarker).then((res) => {
+              let feedbackText = `Marker as been set to ${location}`
+              setFeedback(feedbackText, 'success')
+              mapFlyTo(updatedMarker)
+            })
+          }
+        })
+        // .then((res) => {
+        // })
+        .catch((err) => {
+          console.error(err)
+        })
+    })
   })
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
-  greetingMessage()
+  initSync()
   initMap()
+  initFastMarker()
+  greetingMessage()
 })
--- app/js/script.js
+++ app/js/script.js
@@ -1,42 +1,92 @@
 const greetingMessage = () => {
-  console.log('  #####   ')
-  console.log(' #######  ')
-  console.log('#  ###  # ' + ' Hello There')
-  console.log('#   #   # ')
-  console.log('######### ' + ' Come contribute to the code !')
-  console.log(' ### ###  ')
-  console.log('  #####   ' + ' github.com/SkullMasher/skullfinder')
-  console.log('  # # #   ')
+  console.log(`  #####   `)
+  console.log(` #######  `)
+  console.log(`#  ###  #  Hello There`)
+  console.log(`#   #   # `)
+  console.log(`#########  Come contribute to the code !`)
+  console.log(` ### ###  `)
+  console.log(`  #####    github.com/SkullMasher/skullfinder`)
+  console.log(`  # # #   `)
 }
 
-let initMap = () => {
-  // const skullLatLng = [43.531127, 5.446]
-  const frCenter = [46.9048494736673, 2.61474609375]
-  let aix = new L.LatLng(43.531127, 5.446)
-  let paris = new L.LatLng(48.8567605687911, 2.34334945678711)
-  const skullOnRoad = [aix, paris]
+let map
+let skullMarker
+
+const db = {
+  'remote': '127.0.0.1:5984',
+  'name': 'skullfinder_marker'
+}
+
+// Init the local and remote database
+const localDb = new PouchDB(db.name)
+const remoteDb = `http://${db.remote}/${db.name}`
+
+let initSync = () => {
+  return localDb.sync(remoteDb, {
+    live: true,
+    retry: true
+  }).on('change', (info) => {
+    // handle change
+    console.log('Change Information : ', info)
+  }).on('paused', (err) => {
+    // replication paused (e.g. replication up to date, user went offline)
+    console.log('Paused (replication up to date, user went offline) : ', err)
+  }).on('active', () => {
+    // replicate resumed (e.g. new changes replicating, user went back online)
+    console.log('Active')
+  }).on('denied', (err) => {
+    // a document failed to replicate (e.g. due to permissions)
+    console.log('Denied : ', err)
+  }).on('complete', (info) => {
+    // handle complete
+    console.log('Complete info : ', info)
+  }).on('error', (err) => {
+    // handle error
+    console.log('err : ', err)
+  })
+}
+
+let mapFlyTo = (location) => {
+  skullMarker.setLatLng([location.lat, location.lng])
+    .bindPopup(location.text)
+    .openPopup()
+
+  map.flyTo([lat, lng], 13)
+}
+
+function initMap (res) {
+  const lat = res.lat
+  const lng = res.lng
+  const txt = res.text
+  // const frCenter = [46.9048494736673, 2.61474609375]
+  // const skullOnRoad = [aix, paris]
   // const markerText = 'Florian est en route pour Paris !'
 
-  let map = L.map('map', {
+  map = L.map('map', {
     zoomControl: true
-  }).setView(frCenter, 6)
+  }).setView([lat, lng], 8)
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
   }).addTo(map)
 
-  L.marker(paris).addTo(map)
+  skullMarker = L.marker([lat, lng]).addTo(map)
+    .bindPopup(txt)
+    .openPopup()
 
-  let line = new L.Polyline(skullOnRoad, {
-    color: 'red',
-    weight: 3,
-    opacity: 0.5,
-    smoothFactor: 1
-  })
-  line.addTo(map)
+  // let line = new L.Polyline(skullOnRoad, {
+  //   color: 'red',
+  //   weight: 3,
+  //   opacity: 0.5,
+  //   smoothFactor: 1
+  // })
+  // line.addTo(map)
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
   greetingMessage()
-  initMap()
+  // initSync()
+  localDb.get('currentMarker')
+    .then(initMap)
+    .catch(err => console.log(err))
 })
